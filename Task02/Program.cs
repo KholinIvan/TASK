8f991c5befb441c2bc85ed4c7fcf3325d7a8e20e
Kholin Ivan <ivan.kholin@gmail.com>
2022-11-02 22:35:19 +0300

    upgrade the task02

--- Task02/Program.cs
+++ Task02/Program.cs
@@ -5,7 +5,7 @@ int num1 = Convert.ToInt32(Console.ReadLine());
 int num2 = Convert.ToInt32(Console.ReadLine());
 if (num1 == num2 * num2)
 {
-    Console.WriteLine("Первое число является квадратом второго числа");
+    Console.WriteLine("ДА, первое число является квадратом второго числа");
 }
 else
 {
