ba29e18f2b7d1352f7362b429192358f820e53e6
Kholin Ivan <ivan.kholin@gmail.com>
2022-11-02 22:32:16 +0300

    add the task02

---
+++ Task02/Program.cs
@@ -0,0 +1,13 @@
+﻿// Принимает на вход два числа и проверяет, является ли первое число квадратом второго.
+
+Console.WriteLine("Введите два числа: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+int num2 = Convert.ToInt32(Console.ReadLine());
+if (num1 == num2 * num2)
+{
+    Console.WriteLine("Первое число является квадратом второго числа");
+}
+else
+{
+    Console.WriteLine("Первое число НЕ является квадратом второго числа");
+}
