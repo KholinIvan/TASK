63fde55ddab344db681b35d5c64baad01a794bd2
Kholin Ivan <ivan.kholin@gmail.com>
2022-11-02 22:27:43 +0300

    add the task01

---
+++ Task01/Program.cs
@@ -0,0 +1,14 @@
+﻿// 1. Программа на вход принимает число и
+// 2. выдаёт квадрат этого числа
+// Например:
+// 4 -> 16
+// -3 -> 9
+
+Console.Write("Введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+// string str = Console.ReadLine();
+// int number = Convert.ToInt32(str);
+
+int square = number * number;
+Console.WriteLine($"Квадрат числа {number} = {square}");
