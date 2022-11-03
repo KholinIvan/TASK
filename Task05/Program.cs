adb84c801bd465530371728007dae080183c00b4
Kholin Ivan <ivan.kholin@gmail.com>
2022-11-03 08:13:47 +0300

    UG 1.1

--- Task05/Program.cs
+++ Task05/Program.cs
@@ -4,5 +4,9 @@
 Console.WriteLine("Введите целое трёхзначное число: ");
 int number = Convert.ToInt32(Console.ReadLine());
 
+if (number > 99 && number < 1000)
+{
 int lastDigit = number % 10; // 387 % 10 = 7 (деление с остатком, 387/10 = 38,7 -> остаток 7, что нам и нужно)
 Console.WriteLine($"Последняя цифра числа {number}: {lastDigit}");
+}
+else Console.WriteLine("Вы ввели не трёхзначное число");
