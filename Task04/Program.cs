6de160932a4f2009e98219c03112c6439e57ddd3
Kholin Ivan <ivan.kholin@gmail.com>
2022-11-03 07:50:39 +0300

    add the task04

---
+++ Task04/Program.cs
@@ -0,0 +1,17 @@
+﻿// Вводит натуральное число и выводит все значения от - этого числа до + этого числа:
+// 4 -> -4 -3 -2 -1 0 1 2 3 4 
+
+Console.WriteLine("Введите целое число больше нуля: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 0)
+{
+    int count = -number;
+    while (count <= number)
+    {
+        Console.Write($"{count} ");
+        count++;
+        // count = count + 1;
+    }
+}
+else Console.WriteLine("Введите целое положительное число!");
