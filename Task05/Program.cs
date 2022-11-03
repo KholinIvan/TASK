1cf2391f7470565d615edc3753f84860d1445bdb
Kholin Ivan <ivan.kholin@gmail.com>
2022-11-03 08:06:45 +0300

    add the task05

---
+++ Task05/Program.cs
@@ -0,0 +1,8 @@
+﻿// Вводит трёхзначное число, выводит последнюю цифру этого числа.
+// 387 -> 7
+
+Console.WriteLine("Введите целое трёхзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int lastDigit = number % 10; // 387 % 10 = 7 (деление с остатком, 387/10 = 38,7 -> остаток 7, что нам и нужно)
+Console.WriteLine($"Последняя цифра числа {number}: {lastDigit}");
