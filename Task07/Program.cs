51c0d1cb5865c33270c9df23e50b7d02383624f7
Kholin Ivan <ivan.kholin@gmail.com>
2022-11-06 19:08:10 +0300

    add the Task07

---
+++ Task07/Program.cs
@@ -0,0 +1,15 @@
+﻿// Выводит случайное трёхзначное число и
+// удаляет вторую цифру этого числа.
+
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+
+int number = new Random().Next(100, 1000);
+Console.WriteLine($"Случайное трёхзначное число -> {number}");
+
+int Digit1 = number / 100;
+int Digit3 = number % 10;
+int newNumber = Digit1 * 10 + Digit3;
+
+Console.WriteLine($"Число без второй цифры -> {newNumber}");
