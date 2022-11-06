23304488628eadb730f531b932e4fb24fc5ccf2c
Kholin Ivan <ivan.kholin@gmail.com>
2022-11-06 19:21:43 +0300

    add a method

--- Task07/Program.cs
+++ Task07/Program.cs
@@ -8,8 +8,18 @@
 int number = new Random().Next(100, 1000);
 Console.WriteLine($"Случайное трёхзначное число -> {number}");
 
-int Digit1 = number / 100;
-int Digit3 = number % 10;
-int newNumber = Digit1 * 10 + Digit3;
+// int Digit1 = number / 100;
+// int Digit3 = number % 10;
+// int newNumber = Digit1 * 10 + Digit3;
 
+// Console.WriteLine($"Число без второй цифры -> {newNumber}");
+
+int DeleteSecondDigit(int num)
+{
+    int Digit1 = num / 100;
+    int Digit3 = num % 10;
+    return Digit1 * 10 + Digit3;
+}
+
+int newNumber = DeleteSecondDigit(number);
 Console.WriteLine($"Число без второй цифры -> {newNumber}");
