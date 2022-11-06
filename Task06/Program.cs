bc1cd2ac3d7c04004f087eea68e087b9f0b5f521
Kholin Ivan <ivan.kholin@gmail.com>
2022-11-06 18:54:40 +0300

    end

--- Task06/Program.cs
+++ Task06/Program.cs
@@ -28,10 +28,10 @@ Console.WriteLine($"Случайное число из отрезка 10-99 -> {
 // Console.WriteLine(firstDigit > secondDigit ? firstDigit : secondDigit); // без использования переменной int maxDigit
 
 
-int MaxDigit(int number)
+int MaxDigit(int num)
 {
-    int firstDigit = number / 10;
-    int secondDigit = number % 10;
+    int firstDigit = num / 10;
+    int secondDigit = num % 10;
     return firstDigit > secondDigit ? firstDigit : secondDigit;
 }
 
