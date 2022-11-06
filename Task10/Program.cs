67182e79e7eb89c72e03cf4ee706337c9984a4c9
Kholin Ivan <ivan.kholin@gmail.com>
2022-11-06 20:06:31 +0300

    add the Task10

---
+++ Task10/Program.cs
@@ -0,0 +1,13 @@
+﻿// Принимает на вход два числа и проверяет является ли одно число квадратом другого
+
+// 5, 25 -> да
+// -4, 16 -> да
+// 25, 5 -> да
+// 8, 9 -> нет
+
+Console.WriteLine("Введите два целых числа");
+int num1 = Convert.ToInt32(Console.ReadLine());
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 / num2 == num2 || num2 / num1 == num1) Console.WriteLine("ДА");
+else Console.WriteLine("НЕТ");
