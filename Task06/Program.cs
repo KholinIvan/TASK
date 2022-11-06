e2f00e9fe25fcb2c7797cd38d5689bdc0144266e
Kholin Ivan <ivan.kholin@gmail.com>
2022-11-06 16:09:11 +0300

    add the task06

---
+++ Task06/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 9. Выводит случайное число из отрезка [10, 99] и показывает
+// наибольшую цифру числа:
+// 
+// 78 -> 8
+// 12 -> 2
+// 85 -> 8
+
+int number = new Random().Next(10, 100); // последнее число не включается в диапазон, поэтому ставим 100
+Console.WriteLine($"Случайное число из отрезка 10-99 -> {number}");
+
+int firstDigit = number / 10; // 78 / 10 =7 (целочисленное деление, т.к. переменная у нас int)
+int secondDigit = number % 10; // 78 % 10 = 8 (деление с остатком)
+
+if(firstDigit > secondDigit) Console.WriteLine($"Наибольшая цифра числа равна {firstDigit}");
+else Console.WriteLine($"Наибольшая цифра числа равна {secondDigit}");
+
+// int maxDigit = firstDigit > secondDigit ? firstDigit : secondDigit; -> тернарный оператор
+// (если условие выполняется, тогда (?) переменной присваивается первое значение, иначе (:) второе)
+// Console.WriteLine($"Наибольшая цифра числа равна {maxDigit}");
+
+// Console.Write($"Наибольшая цифра числа {number} равна ");
+// Console.WriteLine(firstDigit > secondDigit ? firstDigit : secondDigit); -> без использования переменной int maxDigit
