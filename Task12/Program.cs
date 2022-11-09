c4f192d6d62fc46f6a27fe7818319419c9f4b33c
Kholin Ivan <ivan.kholin@gmail.com>
2022-11-09 15:25:20 +0300

    UG 1

--- Task12/Program.cs
+++ Task12/Program.cs
@@ -1,15 +1,15 @@
 ﻿// По заданному номеру четверти показывает диапазон возможных координат точек в этой четверти (X и Y).
 
 Console.Write("Введите четверть: ");
-int num = Convert.ToInt32(Console.ReadLine());
+string num = Console.ReadLine();
 
-string Quarter(int number)
+string Quarter(string number)
 {
-    if (number == 1) return "X > 0 и Y > 0";
-    if (number == 2) return "X < 0 и Y > 0";
-    if (number == 3) return "X < 0 и Y < 0";   
-    if (number == 4) return "X > 0 и Y < 0";
-    return "Введите корректное значение номера четверти (это цифры: 1, 2, 3 или 4)";
+    if (number == "1") return "X > 0 и Y > 0";
+    if (number == "2") return "X < 0 и Y > 0";
+    if (number == "3") return "X < 0 и Y < 0";   
+    if (number == "4") return "X > 0 и Y < 0";
+    return "Некорректный ввод";
 }
 
 string quarter = Quarter(num);
