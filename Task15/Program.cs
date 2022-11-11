d4d55fa5e3a05c0e41a990b4e66da141fd69ea34
Kholin Ivan <ivan.kholin@gmail.com>
2022-11-11 18:14:37 +0300

    add the Task15

---
+++ Task15/Program.cs
@@ -0,0 +1,25 @@
+﻿// Принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+
+Console.WriteLine("Введите целое положительное число");
+int digit = Convert.ToInt32(Console.ReadLine());
+
+void SumNum (int a)
+
+{
+if (a >= 1) 
+{
+    int sum = 0;
+    for (int i = 1; i <= a; i++)
+    {
+        sum = sum + i;
+    }
+Console.WriteLine($"Сумма чисел от 1 до {a} = {sum}");
+}    
+    
+else Console.WriteLine("Некорректный ввод");
+}
+
+SumNum (digit);
