291b2244f53cdc20e4bff1aa3466335bf1464cbb
Kholin Ivan <ivan.kholin@gmail.com>
2022-11-11 19:47:18 +0300

    add the Task16

---
+++ Task16/Program.cs
@@ -0,0 +1,21 @@
+﻿// Принимает на вход число и выдаёт кол-во цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int Count(int number)
+{
+    int count = 1;
+    for (int i = 0; i < number; i++)
+    {
+        number = number / 10;
+        count++;
+    }
+    return count;
+}
+
+int sum = Count(num);
+Console.WriteLine($"Количество цифр в числе {num} -> {sum}");
