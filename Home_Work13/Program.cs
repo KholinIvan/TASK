f8394156810625aca57da5807f8ea350c117f20c
Kholin Ivan <ivan.kholin@gmail.com>
2022-11-12 15:28:25 +0300

    add the HW13

---
+++ Home_Work13/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 29:
+// Задаёт массив из 8 элементов, заполненный псевдослучайными числами и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+int number = 8;
+
+int[] Array(int num)
+{
+    int[] array = new int[num];
+    Random rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(-100, 101);
+    }
+    return array; 
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]);
+        if (i < array.Length - 1)
+        {
+            Console.Write(", "); 
+        }
+    }
+    Console.Write("]");
+}
+
+int[] newArray = Array(number);
+PrintArray(newArray);
