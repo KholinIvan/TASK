6e0948184b45498eb1258e43dca9cb672e8cce5b
Kholin Ivan <ivan.kholin@gmail.com>
2022-11-12 15:09:24 +0300

    add the HW12

---
+++ Home_Work12/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 27:
+// Принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+number = Math.Abs(number);
+
+int SumNum(int num)
+{
+int count = 0;
+while (num > 0)
+{
+count = count + num % 10;
+num = num / 10;
+}
+return count;
+}
+
+int sum = SumNum(number);
+Console.WriteLine($"Сумма цифр числа {number} = {sum}");
